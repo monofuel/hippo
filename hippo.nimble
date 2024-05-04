79dde2164781a523394e70151164a771d14eaf57
monofuel <monofuel@japura.net>
2024-05-04 15:11:35 -0400

    add test

---
+++ hippo.nimble
@@ -0,0 +1,8 @@
+version     = "0.0.1"
+author      = "Andrew Brower"
+description = "HIP library for Nim"
+license     = "MIT"
+
+srcDir = "src"
+
+requires "nim >= 2.0.0"
--- tests/test.nim
+++ tests/test.nim
@@ -1,3 +1,9 @@
 ## Put your tests here.
 
-import nimtemplate
+import std/[unittest, osproc]
+
+suite "hip tests":
+    test "call_params.nim":
+      # test building call_params.nim example
+      let res = execCmd("nim cpp tests/hip/call_params.nim")
+      assert res == 0
