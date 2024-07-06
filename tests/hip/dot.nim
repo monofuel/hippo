52e3ab2240bb0c5e0a24b87e13dcf0cb985919d4
monofuel <monofuel@japura.net>
2024-07-06 16:47:39 -0400

    dot product working, but hacky

--- tests/hip/dot.nim
+++ tests/hip/dot.nim
@@ -1,4 +1,4 @@
-import hippo
+import hippo, std/strformat
 
 # GPU Dot product
 
@@ -12,23 +12,28 @@ const
 
 proc dot(a, b, c: ptr[cfloat]){.hippoGlobal.} =
   
-  var cache {.hippoShared.}: ptr[cfloat]
+  #var cache {.hippoShared.}: ptr[cfloat]
+  #var cache {.importc: "cache"}: ptr[cfloat]
+  # __shared__ float cache[threadsPerBlock
+  {.emit:["""__shared__ float cache[256];"""].}
+  let cache {.codeGenDecl:"" importc noinit.}: ptr UncheckedArray[cfloat]
+  #var cache {.hippoShared.}: ptr[cfloat]
 
   # TODO figure out how to do this properly
   let aArray = cast[ptr UncheckedArray[cfloat]](a)
   let bArray = cast[ptr UncheckedArray[cfloat]](b)
   let cArray = cast[ptr UncheckedArray[cfloat]](c)
-  let cacheArray = cast[ptr UncheckedArray[cfloat]](cache)
 
   let cacheIndex = threadIdx.x
   var tid = threadIdx.x + blockIdx.x * blockDim.x
   var temp: cfloat = 0
   while tid < N:
-    temp += aArray[tid] * bArray[tid]
+    # TODO not sure how to handle functions like `pluseq___dot_u17` with nim / cuda
+    temp = temp + (aArray[tid] * bArray[tid])
     tid += blockDim.x * gridDim.x
   
   # set the cache values
-  cacheArray[cacheIndex] = temp
+  cache[cacheIndex] = temp
 
   # synchronize threads in this block
   hippoSyncthreads()
@@ -38,31 +43,32 @@ proc dot(a, b, c: ptr[cfloat]){.hippoGlobal.} =
   var i = blockDim.x div 2
   while i != 0:
     if cacheIndex < i:
-      cacheArray[cacheIndex] += cacheArray[cacheIndex + i]
+      # TODO not sure how to handle functions like `pluseq___dot_u17` with nim / cuda
+      cache[cacheIndex] = cache[cacheIndex] + cache[cacheIndex + i]
     hippoSyncthreads()
     i = i div 2
 
   if cacheIndex == 0:
-    cArray[blockIdx.x] = cacheArray[0]
+    cArray[blockIdx.x] = cache[0]
 
 
 proc main() =
-  var a, b, partial_c: array[N, int32]
+  var a, b, partial_c: array[N, cfloat]
   var dev_a, dev_b, dev_partial_c: pointer
 
   # allocate gpu memory
-  handleError(hipMalloc(addr dev_a, sizeof(float)*N))
-  handleError(hipMalloc(addr dev_b, sizeof(float)*N))
-  handleError(hipMalloc(addr dev_partial_c, BlocksPerGrid * sizeof(float)))
+  handleError(hipMalloc(addr dev_a, sizeof(cfloat)*N))
+  handleError(hipMalloc(addr dev_b, sizeof(cfloat)*N))
+  handleError(hipMalloc(addr dev_partial_c, BlocksPerGrid * sizeof(cfloat)))
 
   # fill in host memory with data
   for i in 0 ..< N:
-    a[i] = i.int32
-    b[i] = (i * 2).int32
+    a[i] = i.float
+    b[i] = (i * 2).float
 
   # copy data to device
-  handleError(hipMemcpy(dev_a, addr a[0], sizeof(float)*N, hipMemcpyHostToDevice))
-  handleError(hipMemcpy(dev_b, addr b[0], sizeof(float)*N, hipMemcpyHostToDevice))
+  handleError(hipMemcpy(dev_a, addr a[0], sizeof(cfloat)*N, hipMemcpyHostToDevice))
+  handleError(hipMemcpy(dev_b, addr b[0], sizeof(cfloat)*N, hipMemcpyHostToDevice))
 
   # launch kernel
   handleError(launchKernel(
@@ -73,18 +79,17 @@ proc main() =
   ))
 
   # copy memory back from GPU to CPU
-  handleError(hipMemcpy(addr partial_c[0], dev_partial_c, BlocksPerGrid * sizeof(float), hipMemcpyDeviceToHost))
+  handleError(hipMemcpy(addr partial_c[0], dev_partial_c, BlocksPerGrid * sizeof(cfloat), hipMemcpyDeviceToHost))
 
   # finish up on the CPU
-  var c: int32 = 0
+  var c: cfloat = 0
   for i in 0 ..< BlocksPerGrid:
     c += partial_c[i]
 
-
-  proc sum_squares(x: float): float =
+  proc sum_squares(x: cfloat): cfloat =
     result = x * (x + 1) * (2 * x + 1) / 6
   
-  echo "Does GPU value ", c.float, " = ", 2 * sum_squares((N - 1)), "?"
+  echo fmt"Does GPU value {c:e} = {2 * sum_squares((N - 1)):e}?"
 
   handleError(hipFree(dev_a))
   handleError(hipFree(dev_b))
