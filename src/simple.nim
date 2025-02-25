3f274576abb284cca0c68186d3a1d43e7f1edb16
monofuel <monofuel@japura.net>
2025-02-25 16:40:47 -0500

    simple hippo pure nim backend with and without threads

--- src/simple.nim
+++ src/simple.nim
@@ -1,16 +1,13 @@
 ## `simple` is a pure-nim backend for hippo.
-## You must compile with --threads:on for using more gpu cores.
+## You must compile with --threads:on for using more cpu cores.
 ## Otherwise it will only use 1 core.
+## Single threaded mode should 'just work' anywhere, even in js, but won't be fast.
 import std/[cpuinfo, macros]
 
 const SingleThread = defined(js) or not compileOption("threads")
 
 when SingleThread:
-  echo "singlethread"
   {.warning: "hippo/simple: Compiled without --threads:on, performance will be limited to 1 core".}
-else:
-  echo "multithread"
-  {.warning: "hippo/simple: Compiling with --threads:on".}
 
 type
   Dim3* = object
@@ -108,10 +105,9 @@ when SingleThread:
 
 else:
 
-  proc worker(closure: proc () {.closure.}) {.thread.} =
+  proc worker(closure: proc () {.closure, gcsafe.}) {.thread.} =
     ## Worker procedure that executes the provided closure in a thread.
-    {.gcsafe.}:
-      closure()
+    closure()
 
   template simpleLaunchKernel(fn: untyped, gridDim: Dim3, blockDim: Dim3, args: tuple) =
     # Multi-threaded execution
@@ -121,16 +117,14 @@ else:
 
     var threadHandles: seq[Thread[proc () {.closure.}]]
     threadHandles.setLen(threads)
-    var startBlock = 0.uint
 
-    for i in 0..<threads.uint:
-      let numBlocks = if i < extraBlocks: blocksPerThread + 1 else: blocksPerThread
-      let endBlock = startBlock + numBlocks
-      proc threadClosure() {.closure.} =
+    proc makeClosure(tid: uint, startBlock: uint, endBlock: uint): proc() {.closure, gcsafe.} =
+      result = proc() {.closure.} =
+        # echo "Thread ", tid, " startBlock=", startBlock, " endBlock=", endBlock
         for bz in 0..<gridDim.z:
           for by in 0..<gridDim.y:
             for bx in 0..<gridDim.x:
-              let blockIndex = bx + by * gridDim.x + bz * gridDim.x * gridDim.y
+              let blockIndex = (bx.uint + by.uint * gridDim.x + bz.uint * gridDim.x * gridDim.y).uint
               if blockIndex >= startBlock and blockIndex < endBlock:
                 blockIdx.x = bx
                 blockIdx.y = by
@@ -142,8 +136,15 @@ else:
                       threadIdx.y = ty
                       threadIdx.z = tz
                       unpackCall(fn, args)
-      createThread(threadHandles[i], worker, threadClosure)
-      startBlock = endBlock
+
+    var startBlock: uint = 0
+    for i in 0..<threads.uint:
+      let numBlocks = if i < extraBlocks: blocksPerThread + 1 else: blocksPerThread
+      let myStartBlock = startBlock
+      let myEndBlock = startBlock + numBlocks
+      let closure = makeClosure(i, myStartBlock, myEndBlock)
+      createThread(threadHandles[i], worker, closure)
+      startBlock = myEndBlock
 
     for th in threadHandles:
       joinThread(th)
