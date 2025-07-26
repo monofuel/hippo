4efa504c8ad27b1288189dc4aae909f92d4e0340
monofuel <monofuel@japura.net>
2025-07-26 15:49:54 -0400

    fix nvidia non-free

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       devShells.${system} = {
