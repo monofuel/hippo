5649b5f03983ca7c7e180bb659c0c5fd74af854f
monofuel <monofuel@japura.net>
2026-01-31 19:43:17 -0500

    multi stream

--- tests/test_streams.nim
+++ tests/test_streams.nim
@@ -107,5 +107,90 @@ suite "Streams":
     hippoStreamDestroy(stream)
 
   test "multi stream test":
-    echo "TODO: Multi-stream test not implemented yet"
-    skip()
+    when HippoRuntime == "SIMPLE":
+      skip()
+
+    var props: HippoDeviceProp
+    let device = hippoGetDevice()
+    hippoGetDeviceProperties(props, device)
+    if props.deviceOverlap == 0:
+      skip()
+
+    let stream0 = hippoStreamCreate()
+    let stream1 = hippoStreamCreate()
+
+    let startEvent = hippoEventCreate()
+    let stopEvent = hippoEventCreate()
+
+    var dev_a0 = hippoMalloc(sizeof(cint) * N)
+    var dev_b0 = hippoMalloc(sizeof(cint) * N)
+    var dev_c0 = hippoMalloc(sizeof(cint) * N)
+    var dev_a1 = hippoMalloc(sizeof(cint) * N)
+    var dev_b1 = hippoMalloc(sizeof(cint) * N)
+    var dev_c1 = hippoMalloc(sizeof(cint) * N)
+
+    let host_a_raw = hippoHostAlloc(sizeof(cint) * FULL_DATA_SIZE)
+    let host_b_raw = hippoHostAlloc(sizeof(cint) * FULL_DATA_SIZE)
+    let host_c_raw = hippoHostAlloc(sizeof(cint) * FULL_DATA_SIZE)
+    var host_a = cast[ptr UncheckedArray[cint]](host_a_raw)
+    var host_b = cast[ptr UncheckedArray[cint]](host_b_raw)
+    var host_c = cast[ptr UncheckedArray[cint]](host_c_raw)
+
+    for i in 0..<FULL_DATA_SIZE:
+      host_a[i] = rand(1000).cint
+      host_b[i] = rand(1000).cint
+
+    hippoEventRecord(startEvent)
+
+    for i in countup(0, FULL_DATA_SIZE - (N * 2), N * 2):
+      hippoMemcpyAsync(dev_a0.p, addr host_a[i], sizeof(cint) * N, hipMemcpyHostToDevice, stream0)
+      hippoMemcpyAsync(dev_a1.p, addr host_a[i + N], sizeof(cint) * N, hipMemcpyHostToDevice, stream1)
+      hippoMemcpyAsync(dev_b0.p, addr host_b[i], sizeof(cint) * N, hipMemcpyHostToDevice, stream0)
+      hippoMemcpyAsync(dev_b1.p, addr host_b[i + N], sizeof(cint) * N, hipMemcpyHostToDevice, stream1)
+
+      hippoLaunchKernel(
+        addKernel,
+        gridDim = newDim3((N div BlockSize).uint32),
+        blockDim = newDim3(BlockSize.uint32),
+        stream = stream0,
+        args = hippoArgs(dev_a0.p, dev_b0.p, dev_c0.p)
+      )
+      hippoLaunchKernel(
+        addKernel,
+        gridDim = newDim3((N div BlockSize).uint32),
+        blockDim = newDim3(BlockSize.uint32),
+        stream = stream1,
+        args = hippoArgs(dev_a1.p, dev_b1.p, dev_c1.p)
+      )
+
+      hippoMemcpyAsync(addr host_c[i], dev_c0.p, sizeof(cint) * N, hipMemcpyDeviceToHost, stream0)
+      hippoMemcpyAsync(addr host_c[i + N], dev_c1.p, sizeof(cint) * N, hipMemcpyDeviceToHost, stream1)
+
+    hippoEventRecord(stopEvent)
+
+    hippoStreamSynchronize(stream0)
+    hippoStreamSynchronize(stream1)
+    hippoEventSynchronize(stopEvent)
+
+    let elapsedTime = hippoEventElapsedTime(startEvent, stopEvent)
+    check elapsedTime >= 0.0
+
+    var correct = 0
+    for i in 0..<min(10, FULL_DATA_SIZE):
+      let idx1 = (i + 1) mod 256
+      let idx2 = (i + 2) mod 256
+      let as_val = (host_a[i].float + host_a[idx1].float + host_a[idx2].float) / 3.0f
+      let bs_val = (host_b[i].float + host_b[idx1].float + host_b[idx2].float) / 3.0f
+      let expected = ((as_val + bs_val) / 2.0f).cint
+      if host_c[i] == expected:
+        correct += 1
+
+    check correct == min(10, FULL_DATA_SIZE)
+
+    hippoHostFree(host_a_raw)
+    hippoHostFree(host_b_raw)
+    hippoHostFree(host_c_raw)
+    hippoEventDestroy(startEvent)
+    hippoEventDestroy(stopEvent)
+    hippoStreamDestroy(stream0)
+    hippoStreamDestroy(stream1)
